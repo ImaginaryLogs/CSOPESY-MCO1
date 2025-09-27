cb9bb1802a0ace4a885e85f7e02590d1c3d159ab
Christian Joseph Bunyi <cjb.nyi@gmail.com>
2025-09-27 20:56:06 +0800

    feat: update compile script to cater to windows

--- scripts/compile.bash
+++ scripts/compile.bash
@@ -1,3 +1,16 @@
+# Use .exe on Windows, otherwise just 'app'
+if [[ "$OS" == "Windows_NT" ]] || [[ "$(uname -s)" =~ MINGW|MSYS|CYGWIN ]]; then
+    OUTFILE="bin/app.exe"
+else
+    OUTFILE="bin/app"
+fi
+
 g++ -std=c++23 -Wall \
     src/main.cpp \
-    -o bin/app
+    src/os_agnostic/Context.cpp \
+    src/os_agnostic/DisplayHandler.cpp \
+    src/os_agnostic/KeyboardHandler.cpp \
+    src/os_agnostic/CommandHandler.cpp \
+    src/os_agnostic/FileReaderHandler.cpp \
+    src/os_dependent/ScannerLibrary.cpp \
+    -o "$OUTFILE"
