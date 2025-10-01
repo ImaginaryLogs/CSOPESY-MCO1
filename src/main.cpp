dab7dfd2ab56aaebe231d740688ef79113cdacff
mariellajeanne <mariellajeannedellosa@gmail.com>
2025-10-01 21:10:32 +0800

    Added welcome banner.

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,20 @@
 int main() {
   std::ios::sync_with_stdio(false);
   std::cin.tie(nullptr);
+
+  // Welcome banner
+  std::cout << "Welcome to Marquee Console!\n"
+            << "Developers :"
+            << " - Christian Joseph C. Bunyi\n"
+            << " - Roan Cedric V. Campo\n"
+            << " - Enzo Rafael S. Chan\n"
+            << " - Mariella Jeanne A. Dellosa\n"
+            << "Tip        : Type 'help' to see available commands.\n\n"
+            << std::flush;
+
   MarqueeConsole console;
   console.run();
+
   std::cout << "Finished Execution!\n";
   return 0;
 }
